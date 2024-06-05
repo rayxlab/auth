93fdf1306c171b2c236d538e9a25d993a10be6c0
Surya <suryakant.subudhi@outlook.com>
2024-06-05 20:38:03 +0530

    refactor: setup reusable components

--- components/forms/signInForm.tsx
+++ components/forms/signInForm.tsx
@@ -1,7 +1,14 @@
+import { CardWrapper } from "@/components/global/cardWrapper"
+
 export const SignInForm = () => {
     return (
-        <div>
+        <CardWrapper
+            headerLabel="Welcome back"
+            backButtonLabel="Don't have an account?"
+            backButtonHref="/secure/sign-up"
+            showSocial
+        >
             Sign in form
-        </div>
+        </CardWrapper>
     )
 }
