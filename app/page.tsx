dd6d5a06e51c8d39b484ac867c69fbfcb4b0d55a
Surya <suryakant.subudhi@outlook.com>
2024-06-05 10:48:50 +0530

    refactor: UI

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>auth</div>
+    <main className="flex flex-col h-full items-center justify-center bg-anime bg-cover">
+      
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      backgroundImage: {
+        "anime": "url('/images/anime.jpg')",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
