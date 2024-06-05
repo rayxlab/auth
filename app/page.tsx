a2899379850f63b3177a4cbe514754082964509d
Surya <suryakant.subudhi@outlook.com>
2024-06-05 11:03:24 +0530

    reafctor: font (UI)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font  = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+})
 
 export default function Home() {
   return (
     <main className="flex flex-col h-full items-center justify-center bg-anime bg-cover">
-      
+      <div className="space-y-6 text-center">
+        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md", font.className)}>
+           🔐Auth 
+        </h1>
+        <p className="text-white">
+          A simple authentication template
+        </p>
+      </div>
     </main>
   );
 }
