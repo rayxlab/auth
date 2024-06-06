e7cab2a9ea64e8ea4e78f00b3c89f36db528e882
Surya <suryakant.subudhi@outlook.com>
2024-06-06 21:10:16 +0530

    feat: Account creation

--- actions/signup.ts
+++ actions/signup.ts
@@ -3,8 +3,12 @@
 "use server";
 
 
-import { SignUpSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+
+import { SignUpSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/utils/data/user";
 
 export const signup = async (values: z.infer<typeof SignUpSchema>) => {
     const validatedFields = SignUpSchema.safeParse(values); //revalidate on server
@@ -13,5 +17,26 @@ export const signup = async (values: z.infer<typeof SignUpSchema>) => {
         return { error: "Inavlid Fields!" };
     }
 
-    return { success : "Valid" };
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //email check
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error: "Email already taken!"};
+    }
+
+    //create user
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword, //secure it before uploading to database
+        }
+    });
+
+    //TODO: send verfication email
+
+    return { success : "Account created!" };
 }
