60787ac10be2cd597a44bf84997350110c520b1b
Surya <suryakant.subudhi@outlook.com>
2024-06-12 11:46:27 +0530

    feat: added Oauth

--- components/auth/socials.tsx
+++ components/auth/socials.tsx
@@ -1,17 +1,27 @@
 "use client";
 
 import { FcGoogle } from "react-icons/fc";
-import { Button } from "../ui/button";
 import { FaGithub } from "react-icons/fa";
+import { signIn } from "next-auth/react";
+
+import { Button } from "../ui/button";
+import { DEFAULT_REDIRECT_ROUTE } from "@/routes";
 
 export const Socials = () => {
+
+    const onClick = (provider: "google" | "github") => {
+        signIn(provider, {
+            callbackUrl: DEFAULT_REDIRECT_ROUTE
+        })
+    }
+
     return (
         <div className="flex items-center w-full gap-x-2">
             <Button
                 size="lg"
                 className="w-full"
                 variant="outline"
-                onClick={() => {}}
+                onClick={() => onClick("google")}
             >
                 <FcGoogle className="h-5 w-5"/>
             </Button>
@@ -19,7 +29,7 @@ export const Socials = () => {
                 size="lg"
                 className="w-full"
                 variant="outline"
-                onClick={() => {}}
+                onClick={() => onClick("github")}
             >
                 <FaGithub className="h-5 w-5"/>
             </Button>
